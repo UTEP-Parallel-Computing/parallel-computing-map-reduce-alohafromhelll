9056dc9de52303b45baa96bcc55fa024b5d5d0ea
root <javelazquez@gmail.com>
2020-02-25 16:24:26 -0700

    fixed code, now working

--- mapreduce.py
+++ mapreduce.py
@@ -2,86 +2,78 @@ import re
 import pymp
 import timeit
 
-docs = []
-doc1 = "the the the the time time time we love love love spent together spent"
 
-#doc2 = "i've spent some time together"
-#with open('shakespeare1.txt', 'r') as f:
-    #doc1 = f.read()
-    #docs.append(doc1)
+#docs = "hate love death night death"
 
-#with open('shakespeare2.txt', 'r') as f:
-    #doc1 = f.read()
-    #docs.append(doc1)
+#read files
+with open('shakespeare1.txt', 'r') as f:
+    doc1 = f.read()
 
-# with open('shakespeare3.txt', 'r') as f:
-#     doc3 = f.read()
-#     docs.append(doc3)
-#
-# with open('shakespeare4.txt', 'r') as f:
-#     doc4 = f.read()
-#     docs.append(doc4)
-#
-# with open('shakespeare5.txt', 'r') as f:
-#     doc5 = f.read()
-#     docs.append(doc5)
-#
-# with open('shakespeare6.txt', 'r') as f:
-#     doc6 = f.read()
-#     docs.append(doc6)
-#
-# with open('shakespeare7.txt', 'r') as f:
-#     doc7 = f.read()
-#     docs.append(doc7)
-#
-# with open('shakespeare8.txt', 'r') as f:
-#     doc8 = f.read()
-#     docs.append(doc8)
-wordlist = [ "hate", "love", "death", "night", "sleep", "time"]
-result = pymp.shared.dict()
+with open('shakespeare2.txt', 'r') as f:
+    doc2 = f.read()
 
-for word in wordlist:
-    result.update({word : 0})
+with open('shakespeare3.txt', 'r') as f:
+    doc3 = f.read()
 
 
+with open('shakespeare4.txt', 'r') as f:
+    doc4 = f.read()
 
-def count_words(wordlist, document, result):
 
-    newlist = re.findall(r"\w+", document)
+with open('shakespeare5.txt', 'r') as f:
+    doc5 = f.read()
 
-    with pymp.Parallel(8) as p:
 
-        #print("hello")
-        #sumLock = p.lock
-        #print(newlist)
+with open('shakespeare6.txt', 'r') as f:
+    doc6 = f.read()
 
-        for w in p.iterate(newlist):
-            #print("hello")
-            if w in result:
-                #sumLock.acquire()
+
+with open('shakespeare7.txt', 'r') as f:
+    doc7 = f.read()
+
+
+with open('shakespeare8.txt', 'r') as f:
+    doc8 = f.read()
+
+
+#combine files into one big string
+docs = doc1 + doc2 + doc3 + doc4 + doc5 + doc6 + doc7 + doc8
+
+wordlist = [ "hate", "love", "death", "night", "sleep", "time",
+            "henry", "hamlet", "you", "my", "blood", "poison", "macbeth",
+            "king", "heart", "honest"
+            ]
+
+
+
+
+def count_words(wordlist, document):
+    result = pymp.shared.dict()
+
+    with pymp.Parallel(16) as p:
+        for word in wordlist:
+            result.update({word : 0})
+
+        #create lock
+        sumLock = p.lock
+
+        for words in p.iterate(wordlist):
+            pattern = re.compile(r''+words+'')
+            if re.findall(pattern,document):
+                length = len(re.findall(pattern,document))
                 #lock
-                result[w] += 1
-                #sumLock.release()
-                #release lock
+                sumLock.acquire()
+                result[words] += length
+                #release
+                sumLock.release()
     return result
-
-
-
-#print(wc)
-#print(docs[0])
-
-
-
-
 
 
 
 #start timer
 start = timeit.default_timer()
 
-#for doc in docs:
-#wordcount = count_words(wordlist, doc1)
-print (count_words(wordlist, doc1, result))
+print (count_words(wordlist, docs))
 #stop timer
 stop = timeit.default_timer()
 #print time
